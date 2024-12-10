e7d39c03166aa90369f72ca4fb716b7380654ac9
shubhangigeektech <shubhangi@geekinformatic.com>
2024-12-10 10:59:11 +0530

    fixed user error

--- src/app/modules/apps/common/common-list/table/columns/RoleCell.tsx
+++ src/app/modules/apps/common/common-list/table/columns/RoleCell.tsx
@@ -10,7 +10,7 @@ const RoleCell: FC<Props> = ({ userObj }) => {
   return (
     <div className='d-flex align-items-center'>
       <div className='d-flex flex-column'>
-        {userObj?.role?.map((r:any,index:number)=>        
+        {userObj && userObj.role && userObj.role.length && userObj?.role?.map((r:any,index:number)=>        
           <span key={index} className='text-gray-800 mb-1 rounded font-sm-12 text-capitalize px-2 bg-gray-300 me-1'>
              {r}
           </span>
